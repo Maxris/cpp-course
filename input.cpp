585617eac81aa9b84e77d24487c9d2f9aa51bf90
Maxris <max.chris.13@outlook.fr>
2024-07-04 12:11:01 +0200

    first input exercise

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int number;
+    std::cout << "input an integer : ";
+
+    std::cin >> number;
+
+    std::cout << "the number is : " << number;
+
+    return 0;
+
+}
